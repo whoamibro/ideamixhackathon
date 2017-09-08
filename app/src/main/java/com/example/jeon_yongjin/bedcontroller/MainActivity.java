322ce222afc1eade1b46f0b8002c2c8fb9b79471
전용진 <jeon-yongjin@jeon-yongjin-ui-MacBook-Pro.local>
2017-09-08 20:42:24 +0900

    framecode update

---
+++ app/src/main/java/com/example/jeon_yongjin/bedcontroller/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.jeon_yongjin.bedcontroller;
+
+import android.app.FragmentTransaction;
+import android.app.FragmentManager;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+    }
+
+}
---
+++ app/src/main/java/com/example/jeon_yongjin/bedcontroller/TimeFragment.java
@@ -0,0 +1,18 @@
+package com.example.jeon_yongjin.bedcontroller;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jeon-yongjin on 2017. 9. 8..
+ */
+
+public class TimeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_time, container, false);
+    }
+}
